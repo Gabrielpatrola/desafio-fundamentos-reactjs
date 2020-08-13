b5a2feb43538c202d68172e296240bfec07b3e18
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-08-12 22:20:29 -0400

    finalizado

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,7 +7,6 @@ interface ContainerProps {
 export const Container = styled.div<ContainerProps>`
   background: #5636d3;
   padding: 30px 0;
-
   header {
     width: 1120px;
     margin: 0 auto;
@@ -15,18 +14,25 @@ export const Container = styled.div<ContainerProps>`
     display: flex;
     align-items: center;
     justify-content: space-between;
-
     nav {
       a {
         color: #fff;
         text-decoration: none;
         font-size: 16px;
         transition: opacity 0.2s;
-
+        position: relative;
+        &.active::after {
+          content: '';
+          position: absolute;
+          height: 2px;
+          background: #ff872c;
+          width: 100%;
+          bottom: -10px;
+          left: 0;
+        }
         & + a {
           margin-left: 32px;
         }
-
         &:hover {
           opacity: 0.6;
         }
